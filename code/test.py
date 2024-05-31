75f82c471d8e7c03bc4f405c14bff66630f3e855
honklin <121810694+honklin@users.noreply.github.com>
2024-05-31 10:06:09 -0400

    Update test.py

--- code/test.py
+++ code/test.py
@@ -4,28 +4,20 @@ import time
 from adafruit_motor import motor
 import pwmio
 import digitalio
-import analogio
 
-pwmA = pwmio.PWMOut(board.GP14, frequency=50)
-pwmB = pwmio.PWMOut(board.GP15, frequency=50)
+pwmA = pwmio.PWMOut(board.GP14)
+pwmB = pwmio.PWMOut(board.GP15)
 motor1 = motor.DCMotor(pwmA,pwmB)
 
-ch1 = digitalio.DigitalInOut(board.GP11)
-ch2 = digitalio.DigitalInOut(board.GP12)
-rot = 0
+ch1 = digitalio.DigitalInOut(board.GP0)
+ch2 = digitalio.DigitalInOut(board.GP1)
+ticks = 0
 old1 = ch1.value
-old2 = ch2.value
 
 while True:
-    motor1.throttle = .5
-
-    if ((ch1.value != old1) or (ch2.value != old2)):
-        rot += 1
-    print(rot/28)
+    motor1.throttle = 1
+    if (ch1.value == False and old1 == True):
+        ticks += 1
     old1 = ch1.value
-    old2 = ch2.value
-
-    #print("ch1:")
-    #print(ch1.value)
-    #print("ch2:")
-    #print(ch2.value)
+    print("rots:")
+    print(ticks/54)
