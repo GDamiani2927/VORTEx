43af74e6c3abf1dc2e23cd8e62281245447b8e53
honklin <121810694+honklin@users.noreply.github.com>
2024-06-01 21:40:39 -0400

    Update Payload.py

--- code/Payload.py
+++ code/Payload.py
@@ -1,18 +1,18 @@
 #type: ignore
 import time
 import board
-import adafruit_mpu6050 #acceleration library
-import busio
+import adafruit_mpu6050 #accelerometer library
+import busio #I2c library
 
-i2c = busio.I2C(board.GP5, board.GP4)
-mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68)
+i2c = busio.I2C(board.GP5, board.GP4) #I2c declaration
+mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68) #accelerometer declaration
 
-with open("/data.csv", "a") as datalog: 
-    while True:
-        runtime = time.monotonic()
-        xacc = mpu.acceleration[0] 
-        yacc = mpu.acceleration[1] 
-        zacc = mpu.acceleration[2] 
+with open("/data.csv", "a") as datalog: #connects datalog to data.csv file
+    while True: #gathers acceleration data
+        runtime = time.monotonic() #total runtime
+        xacc = mpu.acceleration[0] #x acceleration
+        yacc = mpu.acceleration[1] #y acceleration
+        zacc = mpu.acceleration[2] #z acceleration
         datalog.write(f"{runtime},{xacc},{yacc},{zacc}\n") 
-        datalog.flush()
+        datalog.flush() #pushes acceleration data to data.csv
         time.sleep(.25)
